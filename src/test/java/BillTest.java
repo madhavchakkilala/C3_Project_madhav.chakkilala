e04d4ea4e502304eee83524d4676e1de71cd21fb
madhav <madhav.chakkilala@gmail.com>
2021-01-18 23:12:41 +0530

    PART3 : SOLUTION

--- src/test/java/BillTest.java
+++ src/test/java/BillTest.java
@@ -1,16 +1,18 @@
 import org.junit.jupiter.api.BeforeAll;
-
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BillTest {
     Bill bill;
+    List<Item> itemsList;
     @BeforeAll
     public void setup() {
         bill = new Bill();
+        itemsList.add(new item("test",10));
     }
 
     @Test
     public void test_bill_value(){
-        
+        assertEquals(10,bill.calculateBill(itemsList));
     }
 }
