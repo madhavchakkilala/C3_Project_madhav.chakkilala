47b71b07d993ba8bfcc99e95a789165811ae640d
madhav <madhav.chakkilala@gmail.com>
2021-01-18 22:45:55 +0530

    refactored some more code

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -30,6 +30,7 @@ class RestaurantTest {
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
+        assertFalse(restaurant.isRestaurantOpen());
     }
 
     //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
